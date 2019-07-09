c19b6eaf9944c45e1289a79055e368c7c5e9c47a
sabaazimi <526473+gb96@users.noreply.github.com>
2019-07-09 15:51:32 -0400

    connection is added to server and client

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
+const http = require('http');
 const path = require('path');
+const socket = require('socket.io');
+
+let server = http.createServer(app);
+let io = socket(server);  
+
+
 
 
 const publicPath = path.join(__dirname , '/../public');
 app.use(express.static(publicPath));
 
 
+io.on("connection", (socket) => {
+    console.log('new user is connected....');
+    socket.on('disconnect' , () => {
+        console.log('user is disconnected....');
+    })
+})
+
+
+
 
 const port = process.env.PORT || 3000 ;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`listening to port ${port}`);
 })
